e115c405bb237f5628da5d2cd5a45112c43c3776
Marselleze <marsellze.88@gmail.com>
2024-02-14 16:16:26 +0300

    Added last code from lab work

--- 2.1/code/3.cpp
+++ 2.1/code/3.cpp
@@ -4,8 +4,39 @@
 
 using namespace std;
 
-double rec(int x)
+double f(double x, int i, int j){
+    return x * j * 2 + i;
+}
+
+double calculateSum(double x, int n, int i, int j){
+    if (j > n){
+        return 0.0;
+    }
+
+    return f(x, i, j) + calculateSum(x, n, i, j + 1);
+
+    
+}
+double calculateProd(double x, int n, int i){
+    if (i > n){
+        return 1.0;
+    }
+
+    return calculateSum(x, n, i, 1) + calculateProd(x, n, i + 1);
+}
 
 int main(){
+    double x = 0;
+    int n = 0;
 
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Result of programm: " << endl;
+
+    double result = calculateProd(x, n, 0);
+    cout << result;
 }
